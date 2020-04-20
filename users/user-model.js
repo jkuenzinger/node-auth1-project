d0a14a82e54b6d7691b77ed0d2c2dc46950ad5d8
jkuenzinger <jkuenzinger228@gmail.com>
2020-04-20 12:32:23 -0700

    set up standard user-model for use witht he users router i will create next

---
+++ users/user-model.js
@@ -0,0 +1,32 @@
+const db = require('../data/dbConfig.js')
+
+module.exports = {
+    add, 
+    find,
+    findBy,
+    FindById,
+};
+
+function add(user){
+    return db('users').insert(user, 'id')
+    .then(ids => {
+        const [id] = ids;
+        return findById(id)
+    })
+}
+
+function find() {
+    return db('users').select('id', 'username', 'password');
+}
+
+
+function findBy(filter){
+    return db('users').select('id', 'username', 'password').where(filter);
+}
+
+function FindById(id) {
+    return db('users').select('id', 'username').where({id}).first();
+}
+
+
+
